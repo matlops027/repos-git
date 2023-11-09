f433b60f4705e1502afebe961035d2e31a8c6e43
Matheus Lopes <matheus.l.alves@hotmail.com>
2023-11-09 02:41:01 -0300

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import Repositorio from "./pages/Repositorio";
+
+export default function Rotas() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" Component={Main}></Route>
+        <Route
+          exact
+          path="/repositorio/:repositorio"
+          Component={Repositorio}
+        ></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
